e5c0cbc249563ac9f0514483fd33b61a3a05aa39
edcranger <edisonocampo.eo@yahoo.com>
2014-03-22 22:22:16 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 	
+
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>	</title>
+ </head>
+ <body>	
+ 		<ul>
+ 			<li>bricks</li>
+ 			<li>mortar</li>
+
+ 		</ul>
+ 				
+ </body>
+ </html>
